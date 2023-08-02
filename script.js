e0cf9dc8ebc886f7eb5a9a261bce222733b2ad63
tchar <trishitchar@gmail.com>
2023-08-02 21:25:18 +0530

    ok

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x - 10 + "px";
+    crsr.style.top = dets.y - 10 + "px";
+    blur.style.left = dets.x - 250 + "px";
+    blur.style.top = dets.y - 250 + "px";
+
+})
+
 gsap.to("#nav", {
     backgroundColor: "#000",
     duration: 0.5,
